db3bcfa1f5f573574709fe5bcf16b95791564378
Hezekiah <saitoashurin@gmail.com>
2020-02-23 12:44:56 +0800

    new functions

--- app/src/main/java/com/example/timesup_final_project/LoginActivity.java
+++ app/src/main/java/com/example/timesup_final_project/LoginActivity.java
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity{
 
     private TextView regLink;
+    private Button loginButton;
+    private EditText username, password;
+    private final int REG_ACTIVITY_REQ = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,31 @@ public class LoginActivity extends AppCompatActivity{
         regLink = findViewById(R.id.regLink);
         regLink.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 Intent register = new Intent(LoginActivity.this, RegistrationActivity.class);
-                startActivity(register);
+                startActivityForResult(register, REG_ACTIVITY_REQ);
             }
         });
+
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                //go to main activity with intent
+            }
+        });
+
+        username = findViewById(R.id.username_field);
+        password = findViewById(R.id.password_field);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent regIntent){
+        if(requestCode == REG_ACTIVITY_REQ){
+            if (resultCode == AppCompatActivity.RESULT_OK){
+                username.setText(regIntent.getStringExtra("USERNAME"));
+                password.setText(regIntent.getStringExtra("PASSWORD"));
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/timesup_final_project/RegistrationActivity.java
+++ app/src/main/java/com/example/timesup_final_project/RegistrationActivity.java
@@ -1,15 +1,37 @@
 package com.example.timesup_final_project;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistrationActivity extends AppCompatActivity{
 
+    private Button registerButton;
+    private EditText username, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+
+        registerButton = findViewById(R.id.regButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backLogin = new Intent();
+                backLogin.putExtra("USERNAME", username.getText().toString());
+                backLogin.putExtra("PASSWORD", password.getText().toString());
+                setResult(Activity.RESULT_OK, backLogin);
+                finish();
+            }
+        });
     }
 }
