e1b27bedc9b68cd9d1b32d0bbbed5f49cc048328
Hezekiah <saitoashurin@gmail.com>
2020-03-01 19:20:28 +0800

    80 percentodesu

---
+++ app/src/main/java/com/example/timesup_final_project/Deadline.java
@@ -0,0 +1,12 @@
+package com.example.timesup_final_project;
+
+public class Deadline {
+    private String tastTitle, taskDesc, taskDate;
+
+    public String getTastTitle(){return tastTitle;}
+    public void setTastTitle(String tastTitle){this.tastTitle = tastTitle;}
+    public String getTaskDesc(){return taskDesc;}
+    public void setTaskDesc(String taskDesc){this.taskDesc = taskDesc;}
+    public String getTaskDate(){return taskDate;}
+    public void setTaskDate(String taskDate){this.taskDate = taskDate; }
+}
